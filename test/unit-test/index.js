b1e4bb5b0d23f9accbea11695baddc17292c294a
Danver97 <chri.pae@gmail.com>
2020-02-13 23:12:19 +0100

    Missing test calls

--- test/unit-test/index.js
+++ test/unit-test/index.js
@@ -1,3 +1,8 @@
 require('./gender.test');
 require('./phone.test');
 require('./user.test');
+require('./permission.test');
+require('./role.test');
+require('./organization.test');
+require('./repositoryManager.test');
+require('./organizationManager.test');
