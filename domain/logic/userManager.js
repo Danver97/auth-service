7ee8afdf6d51b54de9b5ed1462c9d3bfc75420c8
Danver97 <chri.pae@gmail.com>
2020-02-14 15:31:46 +0100

    User Manager tested

--- domain/logic/userManager.js
+++ domain/logic/userManager.js
@@ -5,22 +5,40 @@ class UserManager {
         this.repo = repo;
     }
 
-    login(userInfo) {
-        const user = new User(userInfo);
+    /**
+     * Login an already created user or register it.
+     * @param {Object} userInfo Users info obtained by a third party issued token
+     * @param {number} userInfo.accountId User's id of the third party
+     * @param {string} userInfo.accountType Third party name
+     * @param {string} userInfo.firstname User's firstname
+     * @param {string} userInfo.lastname User's lastname
+     * @param {string} userInfo.email User's email
+     */
+    async login(userInfo) {
+        let user = new User(userInfo);
         try {
-            await this.getUser(user.uniqueId);
+            user = await this.getUser(user.uniqueId);
         } catch (err) {
             await this.register(user);
         }
         return user;
     }
 
+    /**
+     * Registers a new user
+     * @param {User} user User to register
+     */
     register(user) {
-        return this.repo.register(user);
+        return this.repo.userCreated(user);
     }
 
+    /**
+     * Gets an already registered user
+     * @param {string} userId User id
+     */
     getUser(userId) {
         return this.repo.getUser(userId);
     }
 }
 
+module.exports = UserManager;
