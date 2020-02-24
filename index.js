2694affb28a62cbd1bedf470ce70a2c6545b3a35
Danver97 <chri.pae@gmail.com>
2020-02-24 11:50:05 +0100

    Minor changes

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const ENV = require('./lib/env');
-const app = require('./infrastructure/api/api');
+const repo = require('./infrastructure/repository/repositoryManager')('testdb');
+const queryManagerFunc = require('./infrastructure/query');
+const orgMgr = new (require('./domain/logic/organizationManager'))(repo);
+const userMgr = new (require('./domain/logic/userManager'))(repo);
+const appFunc = require('./infrastructure/api/api');
 
-app.listen(ENV.PORT);
-console.log(`Running on port ${ENV.PORT}`);
+async function run() {
+
+    // FOR TESTING PURPOSES!!!!
+    const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
+    const mongod = new MongoMemoryServer();
+    
+    const mongoOptions = {
+        connString: await mongod.getConnectionString(),
+        dbName: 'auth-service',
+        collectionName: 'auth-service',
+    };
+    queryMgr = await queryManagerFunc(mongoOptions);
+    // !!!!
+
+    
+    app = appFunc(orgMgr, userMgr, queryMgr, 'err');
+    app.listen(ENV.PORT);
+    console.log(`Running on port ${ENV.PORT}`);
+}
+
+run();
